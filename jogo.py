c607c5030f3640cc60c1665d7f07602adc49e8c8
Sophia <sophiavg@al.insper.edu.br>
2026-05-24 22:48:17 -0300

    Começo

--- jogo.py
+++ jogo.py
@@ -20,12 +20,3 @@ FPS = 60
 screen = pygame.display.set_mode((W, H))
 pygame.display.set_caption("O Enigma da Cabana")
 clock = pygame.time.Clock()
-
-# ---------------------------------------------------
-# CORES
-# ---------------------------------------------------
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GOLD = (255, 220, 120)
-GREEN = (80, 220, 120)
-RED = (220, 70, 70)
