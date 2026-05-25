051ede73a1fafaa17af94ea9d4b3f42c90f27613
Sophia <sophiavg@al.insper.edu.br>
2026-05-24 22:50:09 -0300

    Cores, fontes e imagens

--- jogo.py
+++ jogo.py
@@ -20,3 +20,38 @@ FPS = 60
 screen = pygame.display.set_mode((W, H))
 pygame.display.set_caption("O Enigma da Cabana")
 clock = pygame.time.Clock()
+
+# ---------------------------------------------------
+# CORES
+# ---------------------------------------------------
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GOLD = (255, 220, 120)
+GREEN = (80, 220, 120)
+RED = (220, 70, 70)
+
+# ---------------------------------------------------
+# FONTS
+# ---------------------------------------------------
+font = pygame.font.SysFont("arial", 28)
+font_big = pygame.font.SysFont("arial", 42, bold=True)
+font_small = pygame.font.SysFont("arial", 18)
+
+# ---------------------------------------------------
+# IMAGENS
+# ---------------------------------------------------
+ASSET_DIR = Path("cenarios")
+BACKGROUND_PATH = ASSET_DIR / "quarto.png"
+BACKGROUND_POEM_PATH = ASSET_DIR / "quarto_papel.png"
+START_PATH = ASSET_DIR / "inicio.png"
+
+ASSET_DIR_SONS = Path("sons")
+
+background = pygame.image.load(str(BACKGROUND_PATH)).convert()
+background = pygame.transform.scale(background, (W, H))
+
+background_poem = pygame.image.load(str(BACKGROUND_POEM_PATH)).convert()
+background_poem = pygame.transform.scale(background_poem, (W, H))
+
+start_bg = pygame.image.load(str(START_PATH)).convert()
+start_bg = pygame.transform.scale(start_bg, (W, H))
